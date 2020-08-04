178a759d686f5e3f8bbbcf189041604986f06b08
Denis Maciel <denispmaciel@gmail.com>
2020-08-04 04:43:11 +0200

    add audio control

--- config.h
+++ config.h
@@ -1,12 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:size=10" };
-static const char dmenufont[]       = "Ubuntu Mono:size=10";
+static const char *fonts[]          = { "SF Mono:size=10" };
+static const char dmenufont[]       = "SF Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -58,6 +59,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *upvol[]   = { "/usr/bin/pamixer", "-i", "10", NULL };
+static const char *downvol[] = { "/usr/bin/pamixer", "-d", "10", NULL };
+static const char *mutevol[] = { "/usr/bin/pamixer", "-t",   NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -94,6 +98,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
 };
 
 /* button definitions */
